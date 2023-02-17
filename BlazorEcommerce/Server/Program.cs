4ff387b16c4773d159bdaa7e14c4bf8c8b38e28a
jmcmullan92@outlook.com <jmcmullan92@outlook.com>
2023-02-17 22:45:35 +0000

    Adding entity framework, migrations and updating controller method to use datacontext to retrieve Products

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -12,15 +12,9 @@ namespace BlazorEcommerce
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
-            builder.Services.AddDbContext<DataContext>(options => {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
-
-
+            builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
